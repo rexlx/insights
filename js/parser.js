5e92c3b173fd11b0c11eb676588293823f02c056
rexlx <rxlx@nullferatu.com>
2025-06-04 14:19:16 -0500

    fix filename rgx

--- js/parser.js
+++ js/parser.js
@@ -11,12 +11,13 @@ export class Contextualizer {
         "url": /((https?|ftp):\/\/[^\s/$.?#].[^\s]*)/g,
         "domain": /([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/g,
         "filepath": /([a-zA-Z0-9.-]+\/[a-zA-Z0-9.-]+)/g,
-        "filename": /^[\w\-. ]+\.[\w]{2,4}$/g,
+        "filename": /^(?!(\d{1,3}\.){2}\d{1,3}\.[\d]{2,4}$)[\w\-. ]+\.[\w]{2,4}$/g,
     };
     this.context = [];
   }
 
   getMatches(text, regex) {
+    regex.lastIndex = 0;
     let matches = [];
     let match;
     while (match = regex.exec(text)) {
@@ -25,23 +26,3 @@ export class Contextualizer {
     return matches;
   }
 }
-
-// let contextualizer = new Contextualizer();
-// const data = `
-// This is some random text with an MD5 hash: 
-// a1d0c6e83f027327d8461063f4ac58a6. 
-
-// It's just hanging out here, 
-// waiting to be found. 
-
-// Oh, and here's another one: 
-// 8f14e45fceea167a5a36dedd4bea2543. 
-
-// Just a couple of MD5s in the wild. 
-// Carry on... nothing to see here. 
-// `;
-
-// let matches = contextualizer.getMatches(data, contextualizer.expressions.md5);
-// console.log(matches);
-
-// export default Contextualizer;
